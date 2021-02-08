47bd2bb0940a04484df04813b00b884c2488b17b
Scott <scowty.sk@outlook.com>
2021-02-08 12:40:14 +0000

    Update cv2Thermal.py
    HSV space implemented

--- cv2Thermal.py
+++ cv2Thermal.py
@@ -6,28 +6,20 @@ import time
 from scipy import ndimage
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 
-#mlx = seeed_mlx9064x.grove_mxl90640()
-mlx = seeed_mlx9064x.grove_mxl90641()
+#mlx = seeed_mlx9064x.grove_mxl90641()
+mlx = seeed_mlx9064x.grove_mxl90640()
 mlx.refresh_rate = seeed_mlx9064x.RefreshRate.REFRESH_8_HZ  # The fastest for raspberry 4 
-# REFRESH_0_5_HZ = 0b000  # 0.5Hz
-# REFRESH_1_HZ = 0b001  # 1Hz
-# REFRESH_2_HZ = 0b010  # 2Hz
-# REFRESH_4_HZ = 0b011  # 4Hz
-# REFRESH_8_HZ = 0b100  # 8Hz
-# REFRESH_16_HZ = 0b101  # 16Hz
-# REFRESH_32_HZ = 0b110  # 32Hz
-# REFRESH_64_HZ = 0b111  # 64Hz
-#frame = [0]*192
-#frame = np.tile(0,192)
-mlx_shape = (16,12)
+mlx_shape = (24,32)
 
 mlx_interp_val = 10
 mlx_interp_shape = (mlx_shape[0]*mlx_interp_val,
-                    mlx_shape[1]*mlx_interp_val) #new shape(160 by 120)
+                    mlx_shape[1]*mlx_interp_val) #new shape(240 by 320)
 
-fig = plt.figure(figsize=(12,9))#start fig??
+fig = plt.figure(figsize=(4,3))#start fig??
 ax = fig.add_subplot(111)#subplot?
-fig.subplots_adjust(0.05,0.05,0.95,0.95) # get rid of unnecessary padding
+fig.subplots_adjust(0,0,1,1) # get rid of unnecessary padding
+#fig.subplots_adjust(0.002,0.0001,0.9,1)
+#fig.subplots_adjust(0.002,0.02,1,1) # get rid of unnecessary padding
 therm1 = ax.imshow(np.zeros(mlx_interp_shape),interpolation='none',
                    cmap=plt.cm.bwr,vmin=25,vmax=45) # preemptive image
 fig.canvas.draw() # draw figure to copy background
@@ -54,6 +46,43 @@ def crop_image(image, pixel_value=255):
     return cropped_image
 
 t_array = []
+
+##------------------------------HSV SETUP----------------------------------------------------
+def hsv_color_space(image):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    l_h = cv2.getTrackbarPos("L-H","HSV1 BARS")
+    l_s = cv2.getTrackbarPos("L-S","HSV1 BARS")
+    l_v = cv2.getTrackbarPos("L-V","HSV1 BARS")
+    u_h = cv2.getTrackbarPos("U-H","HSV1 BARS")
+    u_s = cv2.getTrackbarPos("U-S","HSV1 BARS")
+    u_v = cv2.getTrackbarPos("U-V","HSV1 BARS")
+    lower_color = np.array([l_h,l_s,l_v])
+    upper_color= np.array([u_h,u_s,u_v])
+    mask = cv2.inRange(hsv,lower_color,upper_color)
+    return mask
+
+def houghLines(img):
+    #threshold1 = cv2.getTrackbarPos("T1","HOUGH1 BARS")
+    #threshold2 = cv2.getTrackbarPos("T2","HOUGH1 BARS")
+    edges = cv2.Canny(img,100,100)
+    return edges
+
+def nothing(X):
+    pass
+
+def createHSVwindow():
+    cv2.namedWindow("HSV1 BARS")
+    cv2.createTrackbar("L-H","HSV1 BARS",15,180, nothing)
+    cv2.createTrackbar("L-S","HSV1 BARS",0,255, nothing)
+    cv2.createTrackbar("L-V","HSV1 BARS",25,180, nothing)
+    cv2.createTrackbar("U-H","HSV1 BARS",68,180, nothing)
+    cv2.createTrackbar("U-S","HSV1 BARS",83,255, nothing)
+    cv2.createTrackbar("U-V","HSV1 BARS",150,255, nothing)
+##-------------------------------------------------------------------------------------------------------
+    
+
+
+createHSVwindow()
 while True:
     t1 = time.monotonic() # for determining frame rate
     try:
@@ -64,8 +93,10 @@ while True:
         plt.axis('image')
         canvas.draw()
         graph_image = np.array(fig.canvas.get_renderer()._renderer)
-        graph_image = crop_image(graph_image, pixel_value=255)
-        graph_image = cv2.cvtColor(graph_image,cv2.COLOR_GRAY2RGB)
+        graph_image = hsv_color_space(graph_image)
+        #graph_image = houghLines(graph_image)
+        #graph_image = crop_image(graph_image, pixel_value=255)
+        #print("shape: ",graph_image.shape)
         cv2.imshow("graph image",graph_image)
     except:
         continue
